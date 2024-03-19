925f9f0ba76d72afc07f45d8b999beee35d7ed3e
cuongdotuan <dotuancuong911@gmail.com>
2024-03-19 22:31:49 +0700

    layout admin page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -12,6 +12,8 @@ import {
   CircularProgress,
   Snackbar,
   Stack,
+  styled,
+  useTheme,
 } from "@mui/material";
 import Categories from "./pages/Categories";
 import {
@@ -21,12 +23,37 @@ import {
 } from "react-router-dom";
 import CreateCategory from "./pages/CreateCategory";
 import EditCategory from "./pages/EditCategory";
+import CreateProduct from "./pages/CreateProduct";
+
+import * as React from "react";
+import { handleLogOut } from "./helper";
+
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import AllInboxIcon from "@mui/icons-material/AllInbox";
+import CategoryIcon from "@mui/icons-material/Category";
+import ReceiptIcon from "@mui/icons-material/Receipt";
 
 const admin = createBrowserRouter([
   { path: "/", element: <Admin /> },
   { path: "/category", element: <Categories /> },
   { path: "/category/create", element: <CreateCategory /> },
   { path: "/category/edit/:id", element: <EditCategory /> },
+  { path: "/product/create/", element: <CreateProduct /> },
+
   { path: "/*", element: <Navigate to="/" replace /> },
 ]);
 const auth = createBrowserRouter([
@@ -34,6 +61,11 @@ const auth = createBrowserRouter([
   { path: "/*", element: <Navigate to="/" replace /> },
 ]);
 
+const drawer = [
+  { id: 1, name: "Products", icon: <AllInboxIcon /> },
+  { id: 2, name: "Categories", icon: <CategoryIcon /> },
+  { id: 3, name: "Oders", icon: <ReceiptIcon /> },
+];
 const AppSnackbar = () => {
   const appContext = useContext(AppContext);
   const { snackbar, setSnackbar } = appContext;
@@ -79,15 +111,144 @@ const AppLoading = () => {
   );
 };
 
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
 function App() {
   const appContext = useContext(AppContext);
   const { user } = appContext;
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
       <AppLoading />
       <AppSnackbar />
-      <RouterProvider router={user ? admin : auth} />
+      {user ? (
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <AppBar position="fixed" open={open}>
+            <Toolbar className="flex justify-between">
+              <Box className="flex items-center">
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={handleDrawerOpen}
+                  edge="start"
+                  sx={{ mr: 2, ...(open && { display: "none" }) }}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" noWrap component="div">
+                  Persistent drawer
+                </Typography>
+              </Box>
+              <Box>
+                <Button
+                  variant="contained"
+                  className="bg-zinc-300"
+                  onClick={handleLogOut}
+                >
+                  logout
+                </Button>
+              </Box>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            sx={{
+              width: drawerWidth,
+              flexShrink: 0,
+              "& .MuiDrawer-paper": {
+                width: drawerWidth,
+                boxSizing: "border-box",
+              },
+            }}
+            variant="persistent"
+            anchor="left"
+            open={open}
+          >
+            <DrawerHeader>
+              <IconButton onClick={handleDrawerClose}>
+                {theme.direction === "ltr" ? (
+                  <ChevronLeftIcon />
+                ) : (
+                  <ChevronRightIcon />
+                )}
+              </IconButton>
+            </DrawerHeader>
+            <Divider />
+            <List>
+              {drawer.map((item, index) => (
+                <ListItem key={item.id} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.name} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+          <Main open={open}>
+            <DrawerHeader />
+
+            <RouterProvider router={admin} />
+          </Main>
+        </Box>
+      ) : (
+        <RouterProvider router={auth} />
+      )}
     </>
   );
 }
