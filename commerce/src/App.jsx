836b3b3eeaca878bdbe47df4ffb7f2e4af357ca7
cuongdotuan <dotuancuong911@gmail.com>
2024-03-24 09:49:23 +0700

    setup commerce page

--- commerce/src/App.jsx
+++ commerce/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Box, Paper, Button } from "@mui/material";
+import Carousel from "react-material-ui-carousel";
+
+import Products from "./pages/Products";
+import Detail from "./pages/Detail";
+import Checkout from "./pages/Checkout";
+
+var carousels = [
+  {
+    url: "../public/slideshow_1_master.jpg",
+  },
+  {
+    url: "../public/slideshow_2.jpg",
+  },
+  {
+    url: "../public/slideshow_3.jpg",
+  },
+  {
+    url: "../public/slideshow_4.jpg",
+  },
+  {
+    url: "../public/slideshow_5.jpg",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  function Item(props) {
+    return (
+      <Paper>
+        <img src={props.item.url} alt="" className="w-full h-auto" />
+      </Paper>
+    );
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Carousel animation="slide" duration={550}>
+        {carousels.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
+      </Carousel>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
