1db418b3502bfad3f8151177b1e8419cba589a2f
- <->
2024-06-27 00:17:36 +0700

    feat: update font-family

--- commerce/src/main.jsx
+++ commerce/src/main.jsx
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import AppProvider from "./context/index.jsx";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: "Garamond, Baskerville, Caslon, serif",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </AppProvider>
   </React.StrictMode>
 );
